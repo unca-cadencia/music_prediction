f7cdd534edefaf1989bc89473b94f85a75f15767
Jeremy Wood <farachan@gmail.com>
2017-12-06 21:15:33 -0500

    Update magenta_generate.sh.

--- magenta_generate.sh
+++ magenta_generate.sh
@@ -1,9 +1,11 @@
-CONFIG=performance
-
-performance_rnn_generate \
---run_dir=/tmp/performance_rnn/logdir/run1 \
---output_dir=/tmp/performance_rnn/generated \
---config=${CONFIG} \
+# Generates the melody from the checkpoints from the model training.
+melody_rnn_generate \
+--run_dir=/.out/melody_rnn/logdir/run1 \
+--output_dir=./out/melody_rnn/generated \
+--config=basic_rnn \
+# How many midis to generate
 --num_outputs=10 \
---num_steps=3000 \
+# How many steps in the music
+--num_steps=128 \
+# The melody to prime the generation with. This is middle C.
 --primer_melody="[60]"
